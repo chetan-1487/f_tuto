bced8067d5e20c3dc98e855f5e7d665c88d323e8
chetan-1487 <chetan@thinksys.com>
2025-11-20 11:05:26 +0530

    minor fix

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
   SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-  SECRET_KEY = os.getenv("DATABASE_URL")
+  SECRET_KEY = os.getenv("SECRET_KEY")
   SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATION")
   JWT_SECRET = os.getenv("JWT_SECRET")
   JWT_ALGO = os.getenv("JWT_ALGO")
